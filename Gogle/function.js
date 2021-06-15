3498b9f0c52b13783afa61e3c9856f1b657f2ab7
SadManKaHou <39438345+SadKaHou@users.noreply.github.com>
2021-06-15 09:32:31 +0800

    Add files via upload

---
+++ Gogle/function.js
@@ -0,0 +1,8 @@
+function Clear() {
+  document.getElementById("search-place").reset();
+  document.getElementById("vanishing-buttons").style.display = "none";
+}
+function Clear2() {
+  document.getElementById("search-place").reset();
+  document.getElementById("clear-section").style.display = "none";
+}
